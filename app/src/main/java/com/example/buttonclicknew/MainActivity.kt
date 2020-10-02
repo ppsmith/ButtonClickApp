7128c4bd37f7c0a47bfddeccd828fc65ebb6adad
psmith <psmith@tradetec.com>
2020-10-02 17:27:24 -0400

    save the state of the list when the phone is rotated. This only saves the state, does not write any memory to disk. Everything is kept on chip

--- app/src/main/java/com/example/buttonclicknew/MainActivity.kt
+++ app/src/main/java/com/example/buttonclicknew/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.buttonclicknew
 
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = ""
+
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -24,12 +30,52 @@ class MainActivity : AppCompatActivity() {
         textView?.movementMethod = ScrollingMovementMethod()
 
         button.setOnClickListener {
+            Log.d(TAG, "On Click: Called")
             textView?.append(userInput.text)
             textView?.append(("\n"))
             userInput.setText("")
         }
 
     }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text =  savedInstanceState.getString(TEXT_CONTENTS, "")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "Saved this: ${textView?.text.toString()}")
+        outState.putString(TEXT_CONTENTS, textView?.text.toString())
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy called")
+        super.onDestroy()
+    }
+
 
 }
 
